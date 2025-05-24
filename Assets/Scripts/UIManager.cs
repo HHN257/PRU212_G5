7d851f1899857f333395febee69baf3c2c2fea5e
HHN257 <hoanghuunam257@gmail.com>
2025-05-24 11:11:47 +0700

    Fix instruction

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,7 @@ public class UIManager : MonoBehaviour
 {
     public GameObject instructionPanel;
     public GameObject background; // Reference to your background GameObject
+    public GameObject highScore; // Reference to your high score GameObject
 
     AudioManagerStartGame audioManager;
     private void Awake()
@@ -16,6 +17,7 @@ public class UIManager : MonoBehaviour
         audioManager.PlaySFX(audioManager.buttonClickSFX); // Play button click sound
         instructionPanel.SetActive(true);
         background.SetActive(false); // Hide background
+        highScore.SetActive(false);
     }
 
     public void HideInstructions()
@@ -23,5 +25,6 @@ public class UIManager : MonoBehaviour
         audioManager.PlaySFX(audioManager.buttonClickSFX); // Play button click sound
         instructionPanel.SetActive(false);
         background.SetActive(true); // Show background again
+        highScore.SetActive(true); // Show high score again
     }
 }
